a0e852272eec203d4e8267e882164da7937b1bc6
Shawn Maisonneuve <maisonneuve.s@hotmail.com>
2023-02-13 04:52:09 -0500

    dbupdate

--- modele/DAO/configs/configBD.interface.php
+++ modele/DAO/configs/configBD.interface.php
@@ -1,10 +1,10 @@
 <?php	
 	interface ConfigBD
 	{	
-		const BD_HOTE = "localhost";
-		const BD_UTILISATEUR = "root";
-		const BD_MOT_PASSE = "root";
-		const BD_NOM = "afterwork";
+		const BD_HOTE = "us-cdbr-east-06.cleardb.net";
+		const BD_UTILISATEUR = "b49452e57eabbf";
+		const BD_MOT_PASSE = "e4f0b442";
+		const BD_NOM = "heroku_ea327247e5469e5";
 	}
 
 ?>
