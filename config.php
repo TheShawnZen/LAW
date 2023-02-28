a7afc8e3992337c0dcdf488fc008c7f8a6dea2dd
Shawn Maisonneuve <maisonneuve.s@hotmail.com>
2023-02-28 06:28:06 -0500

    db test 1

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	$db_host = "us-cdbr-east-06.cleardb.net";
+	$db_user = "b49452e57eabbf";
+	$db_pass = "e4f0b442";
+	$db_name = "heroku_ea327247e5469e5";
+	
+	$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error")
+?>
